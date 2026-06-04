4128995d2af344d7c845acfac0287cfc8e8071c4
Oscar-cm14 <ocabezas@unicauca.edu.co>
2026-06-04 00:38:03 -0500

    Update environment.prod.ts

--- frontend-angular/src/environments/environment.prod.ts
+++ frontend-angular/src/environments/environment.prod.ts
@@ -2,15 +2,15 @@
 // environment.prod.ts — Configuración PRODUCCIÓN
 // Usado con: ng build --configuration production
 //
-// ⚠️  REEMPLAZA las URLs después de desplegar:
+//   REEMPLAZA las URLs después de desplegar:
 //   apiUrl      → URL de tu backend en Render
-//   keycloakUrl → URL de tu Keycloak en Azure
+//   keycloakUrl → URL de tu Keycloak en Render
 // ============================================================
 
 export const environment = {
   production: true,
   // Cambia esto a la URL de tu backend en Render
-  apiUrl:       'https://clinica-backend.onrender.com/api/v1',
+  apiUrl:       'https://clinica-backend-n6ft.onrender.com/api/v1',
   // Cambia esto a la URL de tu Keycloak en Render
-  keycloakUrl:  'https://clinica-keycloak.onrender.com'
+  keycloakUrl:  'https://clinica-keycloak-vzds.onrender.com'
 };
